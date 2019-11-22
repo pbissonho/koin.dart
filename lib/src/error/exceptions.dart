c5c7ad4f5c7901acb8a19c09e3fc6e6588a3e1a4
Pedro Bissonho <pedro.bissonho@gmail.com>
2019-11-22 15:41:08 -0300

    improvements KoinException

--- lib/src/error/exceptions.dart
+++ lib/src/error/exceptions.dart
@@ -1,60 +1,57 @@
+/*
+ * Copyright 2017-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 abstract class KoinException implements Exception {
   final String msg;
-  String get exceptionName;
   KoinException(this.msg);
 
   @override
   String toString() {
-    return "$exceptionName: $msg";
+    return "${this.runtimeType}: $msg";
   }
 }
 
 class BadScopeInstanceException extends KoinException {
   BadScopeInstanceException(String msg) : super(msg);
-
-  String get exceptionName => "BadScopeInstanceException";
 }
 
 class IllegalStateException extends KoinException {
   IllegalStateException(String msg) : super(msg);
-
-  String get exceptionName => "IllegalStateException";
 }
 
 class InstanceCreationException extends KoinException {
-  // Todo
-  // add a exception na message
   InstanceCreationException(String msg, Exception e) : super(msg);
-
-  String get exceptionName => "InstanceCreationException";
 }
 
 class KoinAppAlreadyStartedException extends KoinException {
   KoinAppAlreadyStartedException(String msg) : super(msg);
-
-  String get exceptionName => "KoinAppAlreadyStartedException";
 }
 
 class MissingPropertyException extends KoinException {
   MissingPropertyException(String msg) : super(msg);
-
-  String get exceptionName => "MissingPropertyException";
 }
 
 class DefinitionOverrideException extends KoinException {
   DefinitionOverrideException(String msg) : super(msg);
-
-  String get exceptionName => "DefinitionOverrideException";
 }
 
 class NoParameterFoundException extends KoinException {
   NoParameterFoundException(String msg) : super(msg);
-
-  String get exceptionName => "NoParameterFoundException";
 }
 
 class NoBeanDefFoundException extends KoinException {
   NoBeanDefFoundException(String msg) : super(msg);
-
-  String get exceptionName => "NoParameterFoundException";
 }
