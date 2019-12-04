39fc6b3463fd619bc7cc953172890d2df74a8a84
Pedro Bissonho <pedro.bissonho@gmail.com>
2019-12-04 00:14:33 -0300

    fixed example error

---
+++ README.md
@@ -0,0 +1,15 @@
+# Koin.dart
+
+A pragmatic lightweight dependency injection framework. This is a port of [Koin]https://github.com/InsertKoinIO/koin for Dart projects.
+
+| Package                                                                            | Pub                                                                                                    |
+| ---------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------ |
+| [koin](https://github.com/pbissonho/koin.dart/tree/master/packages/koin)                 | [![pub package](https://img.shields.io/pub/v/koin.svg)](https://pub.dev/packages/koin)                 |
+| [koin_test](https://github.com/pbissonho/koin.dart/tree/master/packages/koin_test)       | [![pub package](https://img.shields.io/pub/v/koin_test.svg)](https://pub.dev/packages/koin_test)       |
+| [koin_flutter](https://github.com/pbissonho/koin.dart/tree/master/packages/koin_flutter) | [![pub package](https://img.shields.io/pub/v/koin_flutter.svg)](https://pub.dev/packages/koin_flutte) |
+
+
+
+## Usage
+
+
---
+++ examples/basic/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:koin/koin.dart';
+import 'package:flutter/material.dart';
+import 'package:koin_flutter/koin_flutter.dart';
+
+var appModule = Module()..single((s, p) => CounterController());
+
+class CounterController {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+  }
+
+  void decrement() {
+    _counter--;
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ScopeProvider.root(
+          child: MyHomePage(title: 'Flutter Demo Home Page'),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with Injector {
+  CounterController counterController;
+
+  @override
+  void initState() {
+    super.initState();
+    startKoin((app) {
+      app.module(appModule);
+    });
+  }
+
+  @override
+  void dispose() {
+    stopKoin();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    counterController = inject<CounterController>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${counterController.counter}',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counterController.increment();
+          });
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
