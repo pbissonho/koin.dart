f89f10fa441d51a7230f2e8b0600a5b77378b959
pbissonho <pedro.bissonho@gmail.com>
2020-04-28 23:16:33 -0300

    added global_to_scope_test

---
+++ packages/koin/test/core/global_to_scope_test.dart
@@ -0,0 +1,53 @@
+import 'package:koin/koin.dart';
+import 'package:koin/src/core/error/exceptions.dart';
+import 'package:test/test.dart';
+
+import '../components.dart';
+
+class ScopeType {}
+
+void main() {
+  test("can't get scoped dependency without scope", () {
+    var koin = koinApplication((app) {
+      app.printLogger(level: Level.debug);
+      app.module(module()
+        ..scope<ScopeType>((s) {
+          s.scoped((s, p) => ComponentA());
+        }));
+    }).koin;
+
+    expect(() => koin.get<ComponentA>(),
+        throwsA((value) => value is NoBeanDefFoundException));
+  });
+
+  test("can't get scoped dependency without scope from single", () {
+    var koin = koinApplication((app) {
+      app.printLogger(level: Level.debug);
+      app.module(module()
+        ..single((s, p) => ComponentB(s.get()))
+        ..scope<ScopeType>((s) {
+          s.scoped((s, p) => ComponentA());
+        }));
+    }).koin;
+
+    expect(() => koin.get<ComponentA>(),
+        throwsA((value) => value is NoBeanDefFoundException));
+  });
+
+  test('get scoped dependency without scope from single', () {
+    var scopeId = 'MY_SCOPE_ID';
+
+    var koin = koinApplication((app) {
+      app.printLogger(level: Level.debug);
+      app.module(module()
+        ..single((s, p) => ComponentB(s.getScope(scopeId).get()))
+        ..scope<ScopeType>((s) {
+          s.scoped((s, p) => ComponentA());
+        }));
+    }).koin;
+
+    var scope = koin.createScope(scopeId, named<ScopeType>());
+
+    expect(koin.get<ComponentB>().a, scope.get<ComponentA>());
+  });
+}
