21e6f66e1ab04f2ef4681809ccd55e40a582e3f7
pbissonho <pedro.bissonho@gmail.com>
2020-05-17 10:05:27 -0300

    added multiple_modules_declaration_test

---
+++ packages/koin/test/core/multiple_modules_declaration_test.dart
@@ -0,0 +1,38 @@
+import 'package:koin/koin.dart';
+import 'package:test/test.dart';
+
+import '../components.dart';
+import '../extensions/koin_application_ext.dart';
+
+void main() {
+  test('run with DI with several modules', () {
+    var app = koinApplication((app) {
+      app.modules([
+        Module()..single((s, p) => ComponentA()),
+        Module()..single((s, p) => ComponentB(s.get()))
+      ]);
+    });
+
+    app.expectDefinitionsCount(2);
+  });
+
+  test('resolve DI with several modules', () {
+    var app = koinApplication((app) {
+      app.modules([
+        Module()..single((s, p) => ComponentA()),
+        Module()..single((s, p) => ComponentB(s.get()))
+      ]);
+    });
+
+    var koin = app.koin;
+
+    var a = koin.get<ComponentA>();
+    var b = koin.get<ComponentB>();
+
+    var aInject = koin.inject<ComponentA>();
+    var bInject = koin.inject<ComponentB>();
+
+    expect(a, b.a);
+    expect(aInject(), bInject().a);
+  });
+}
