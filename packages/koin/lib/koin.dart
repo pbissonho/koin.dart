9e1a3dc633f15966bf1bab292a60c1823f1186c6
pbissonho <pedro.bissonho@gmail.com>
2020-07-12 09:39:40 -0300

    exports(koin) - removed some exports

--- packages/koin/lib/koin.dart
+++ packages/koin/lib/koin.dart
@@ -6,18 +6,14 @@ export 'src/core/context/koin_context_handler.dart';
 export 'src/core/definition/definition.dart';
 
 export 'src/core/definition_parameters.dart';
-export 'src/dsl/definition_binding.dart';
 
 export 'src/core/module.dart';
 export 'src/core/lazy/lazy.dart';
 export 'src/core/logger.dart';
 
-export 'src/core/measure.dart';
-
 export 'src/core/qualifier.dart';
 export 'src/core/scope/scope.dart';
-export 'src/core/definition/bean_definition.dart';
-export 'src/core/definition/options.dart';
+export 'src/core/definition/bean_definition.dart' show BeanDefinition;
 
 export 'src/dsl/scope_dsl.dart';
 export 'src/dsl/module_dsl.dart';
