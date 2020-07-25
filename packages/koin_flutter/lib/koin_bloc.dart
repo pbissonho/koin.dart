45b402babeea3776fd0531ae62be6cc7ab4ed25b
pbissonho <pedro.bissonho@gmail.com>
2020-07-25 11:35:56 -0300

    (koin_flutter)  depreciate bloc extension

---
+++ packages/koin_flutter/lib/koin_bloc.dart
@@ -0,0 +1,1 @@
+export 'src/bloc_extension.dart';
---
+++ packages/koin_flutter/lib/src/bloc_extension.dart
@@ -0,0 +1,83 @@
+import 'package:koin/koin.dart';
+import 'disposable_extension.dart';
+
+extension DisposableModuleExtension on Module {
+  
+  ///Pass to use:
+  ///First import disposable_extension.dart and define a Disposable
+  ///```
+  ///var blocModule = Module()..disposable((s) => Bloc());
+  ///``` 
+  /// With the launch of the "koin_bloc" package, this will have been renamed to avoid confusion.
+  ///
+  /// This will be removed in the next versions, , then use 'disposable'.
+  /// 
+  /// 
+  @deprecated
+  BeanDefinition<T> bloc<T extends Disposable>(
+    DefinitionFunction<T> definition, {
+    Qualifier qualifier,
+    bool createdAtStart = false,
+    bool override = false,
+  }) {
+    var beanDefinition = single<T>(definition,
+        qualifier: qualifier,
+        createdAtStart: createdAtStart,
+        override: override);
+
+    beanDefinition.onClose((bloc) => bloc.dispose());
+    return beanDefinition;
+  }
+
+  ///Pass to use:
+  ///First import disposable_extension.dart and define a Disposable
+  ///```
+  /// Module()..scopeOneDisposable<MyBloc, MyScope>((s) => MyBloc());
+  ///```
+  ///With the launch of the "koin_bloc" package, this will have been renamed to avoid confusion.
+  ///
+  /// This will be removed in the next versions, then use 'scopeOneDisposable'.
+  /// 
+  @deprecated
+  BeanDefinition<T> scopeOneBloc<T extends Disposable, TScope>(
+    DefinitionFunction<T> definition, {
+    Qualifier qualifier,
+    bool createdAtStart = false,
+    bool override = false,
+  }) {
+    final beanDefinition = scopeOne<T, TScope>(definition,
+        qualifier: qualifier,
+        createdAtStart: createdAtStart,
+        override: override);
+    beanDefinition.onClose((value) => value.dispose());
+    return beanDefinition;
+  }
+}
+
+extension ScopeSetDisposableExtension on ScopeDSL {
+  ///Pass to use:
+  ///First import disposable_extension.dart and define a Disposable
+  ///```
+  ///var blocModule = Module()
+  /// ..scope<ScopeWidget>((scope) {
+  ///   scope.scopedDisposable<ScopedBloc>((s) => ScopedBloc());
+  /// });
+  ///```
+  ///
+  /// With the launch of the "koin_bloc" package, this will have been renamed to avoid confusion.
+  ///
+  /// This will be removed in the next versions, then use 'scopedDisposable'.
+  @deprecated
+  BeanDefinition<T> scopedBloc<T extends Disposable>(
+    DefinitionFunction<T> definition, {
+    Qualifier qualifier,
+    bool createdAtStart = false,
+    bool override = false,
+  }) {
+    var beanDefinition =
+        scoped<T>(definition, qualifier: qualifier, override: override);
+
+    beanDefinition.onClose((bloc) => bloc.dispose());
+    return beanDefinition;
+  }
+}
